8e56abefe8abf82a1be737b4b694187c9f08180a
Luo Yi <ext.yi.luo@uaes.com>
2017-09-04 13:58:01 +0800

    modify course

--- application/controllers/Courses.php
+++ application/controllers/Courses.php
@@ -8,6 +8,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Courses extends CI_Controller
 {
+    private $edit_course;
+    private $edit;
+
     public function __construct()
     {
         parent::__construct();
@@ -15,6 +18,9 @@ class Courses extends CI_Controller
 			redirect('login');
 		if ( $this->user->level <= 2) // permission denied
 			show_404();
+
+        $this->edit_course = array();
+        $this->edit = FALSE;
     }
 
     public function index()
@@ -33,35 +39,54 @@ class Courses extends CI_Controller
             show_404();
 
 		$data = array(
+            'edit' => $this->edit,
 			'all_assignments' => $this->assignment_model->all_assignments(),
 		);
 
         $name = $this->input->post('name');
         if ($name === NULL)
         {
+            if ($this->edit)
+            {
+                $data['edit_course'] = $this->course_model->course_info($this->edit_course);
+            }
             $this->twig->display('pages/admin/add_course.twig', $data);
         }
         else
         {
             $registration_code = $this->input->post('registration_code');
-            $this->course_model->add_course($name, $registration_code);
+            if ($this->edit)
+            {
+                $this->course_model->update_course($this->edit_course, $name, $registration_code);
+            }
+            else
+            {
+                $this->course_model->add_course($name, $registration_code);
+            }
             redirect('courses');
         }
     }
 
     public function delete($course_id = FALSE)
     {
-        if ($assignment_id === FALSE)
+        if ($course_id === FALSE)
             show_404();
         if ($this->user->level <= 2)
             show_404();
 
-        if ($this->input->post('delete') === 'delete')
-        {
-            $this->course_model->delete_course($course_id);
-            redirect('courses');
-        }
+        $this->course_model->delete_course($course_id);
+        redirect('courses');
+    }
 
+    public function edit($course_id)
+    {
+        if ($this->user->level <= 2)
+            show_404();
+
+        $this->edit_course = $course_id;
+        $this->edit = TRUE;
+
+        $this->add();
     }
 
 }
--- application/models/Course_model.php
+++ application/models/Course_model.php
@@ -39,6 +39,18 @@ class Course_model extends CI_Model
         return $query->row();
     }
 
+    public function course_info($course_id)
+    {
+        $query = $this->db->get_where('courses', array('id'=>$course_id));
+        if ($query->num_rows() != 1)
+            return array(
+                'id' => 0,
+                'name' => '',
+                'registration_code' => ''
+            );
+        return $query->row_array();
+    }
+
     public function get_all_courses()
     {
         return $this->db->order_by('id')->get('courses')->result_array();
@@ -49,6 +61,22 @@ class Course_model extends CI_Model
         $this->db->trans_start();
 
         $this->db->delete('courses', array('id'=>$course_id));
+
+        $this->db->trans_complete();
+
+        return TRUE;
+    }
+
+    public function update_course($course_id, $name, $registration_code)
+    {
+        $this->db->trans_start();
+
+        $course = array(
+            'name' => $name,
+            'registration_code' => $registration_code
+        );
+
+        $this->db->where('id', $course_id)->update('courses', $course);
 
         $this->db->trans_complete();
 
